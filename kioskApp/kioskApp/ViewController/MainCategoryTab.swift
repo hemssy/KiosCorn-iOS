a2447adb1fcd656cecdedbd64132716534d8c0e4
104hyeon <104hyeon@gmail.com>
2025-09-17 16:16:05 +0900

    [Feat] #5 - 메인화면 상단 카테고리탭 / 하단 주문하기 버튼 추가

---
+++ kioskApp/kioskApp/ViewController/MainCategoryTab.swift
@@ -0,0 +1,8 @@
+//
+//  MainCategoryTab.swift
+//  kioskApp
+//
+//  Created by bakae on 9/17/25.
+//
+
+import Foundation
---
+++ kioskApp/kioskApp/ViewController/MainOrderButton.swift
@@ -0,0 +1,8 @@
+//
+//  MainOrderButton.swift
+//  kioskApp
+//
+//  Created by 박혜연 on 9/17/25.
+//
+
+import Foundation
